9aad4b2f3526f62053be45bae53996b510599df7
JungChoon Park <unlimitedfocus@gmail.com>
2017-11-21 11:27:12 +0900

    [PYTHON] isogram #2

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,8 +1,4 @@
 import re
 def is_isogram(string):
     string = re.sub('[^A-Za-z0-9]+', '', string.lower())
-    size = len(string)
-    if 2 > size:
-        return True
-    else:
-        return size == len(set(string))
+    return len(string) == len(set(string))
