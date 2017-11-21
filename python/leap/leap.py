989750bc2f081bc83024ff349fc9bb7afb6e2718
JungChoon Park <unlimitedfocus@gmail.com>
2017-11-21 11:29:44 +0900

    [PYTHON] leap #3

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,6 +1,2 @@
 def is_leap_year(year):
-    div_4 = (year % 4 == 0)
-    div_100 = (year % 100 == 0)
-    div_400 = (year % 400 == 0)
-
-    return div_4 and ((div_100 and div_400) or (not div_100 and not div_400))
+    return ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)
