98466b05cd8b1949d855432f19749e7d4ecfa9f3
JungChoon Park <unlimitedfocus@gmail.com>
2017-12-20 00:12:15 +0900

    ruby hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,12 +2,11 @@ class Hamming
   def self.compute(dna_a, dna_b)
     raise ArgumentError if dna_a.length != dna_b.length
 
-    dna_a.chars.map.with_index do |char, index|
-      if char != dna_b[index]
-        1
-      else
-        0
-      end
-    end.reduce(:+) || 0
+    index = 0
+    dna_a.chars.count do |x| 
+      result = (x != dna_b[index])
+      index += 1
+      result
+    end
   end
 end
